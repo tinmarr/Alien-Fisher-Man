1f7d0fa3091b19dece764d719c73431baa31c4c9
mrElnekave <klavlavigugu@gmail.com>
2021-10-15 12:58:06 -0700

    fish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import rubato as rb
+import random
 
 game = rb.Game()
 
@@ -10,3 +11,17 @@ player = rb.RigidBody({
     "pos": rb.Vector(50, 50),
     "debug": True
 })
+
+
+fish = rb.Group()
+mainScene.add(fish)
+
+def gen_fish(top_left: rb.Vector, bottom_right: rb.Vector, amt):
+    fish_imgs = [""]
+    for _ in range(amt):
+        fish.add(rb.RigidBody({
+                "img": random.choice(fish_imgs),
+                "hitbox": rb.Polygon.generate_rect(16, 16),
+                "pos": random.randint(top_left.x, bottom_right.x), random.randint(top_left.y, bottom_right.y),
+                "debug": True
+            })
